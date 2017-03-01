e1f2536840aa48d11f37234e3ae1e4eab82805af
Cooper Cain <coopercc@uw.edu>
2017-03-01 09:01:43 -0800

    added fragments

---
+++ ReFlex/app/src/main/java/reflex/ischool/washington/edu/reflex/WorkoutActivity.java
@@ -0,0 +1,13 @@
+package reflex.ischool.washington.edu.reflex;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WorkoutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout);
+    }
+}
