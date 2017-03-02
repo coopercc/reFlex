4d5d811f3ac528adec4a62303fa5ebdc363a6f35
Cooper Cain <coopercc@uw.edu>
2017-03-01 19:16:55 -0800

    Added fragment and custom card view

--- ReFlex/app/src/main/java/reflex/ischool/washington/edu/reflex/WorkoutActivity.java
+++ ReFlex/app/src/main/java/reflex/ischool/washington/edu/reflex/WorkoutActivity.java
@@ -1,6 +1,7 @@
 package reflex.ischool.washington.edu.reflex;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class WorkoutActivity extends Activity {
@@ -9,5 +10,10 @@ public class WorkoutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+
+        WorkoutFragment workoutFrag = new WorkoutFragment();
+        FragmentTransaction tx = getFragmentManager().beginTransaction();
+        tx.replace(R.id.fragment_placeholder, workoutFrag);
+        tx.commit();
     }
 }
--- ReFlex/app/src/main/java/reflex/ischool/washington/edu/reflex/WorkoutFragment.java
+++ ReFlex/app/src/main/java/reflex/ischool/washington/edu/reflex/WorkoutFragment.java
@@ -2,28 +2,72 @@ package reflex.ischool.washington.edu.reflex;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WorkoutFragment extends Fragment {
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<Exercise> exerciseList;
 
     public WorkoutFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        //create view
+
+        recyclerView = (RecyclerView) getView().findViewById(R.id.workoutRecycleView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        exerciseList = new ArrayList<Exercise>();
+
+
+        for (int i = 0; i < 3; i++) {
+            Exercise temp = new Exercise();
+            if (i == 0) {
+                temp.setName("Pushups");
+                temp.setSets(3);
+                temp.setReps(12);
+            } else if (i == 1) {
+                temp.setName("Situps");
+                temp.setSets(3);
+                temp.setReps(12);
+            } else {
+                temp.setName("Pullups");
+                temp.setSets(4);
+                temp.setReps(6);
+            }
+            exerciseList.add(temp);
+        }
+
+        adapter = new workoutAdapter(exerciseList, this.getContext());
+        recyclerView.setAdapter(adapter);
+
+
+
         return inflater.inflate(R.layout.fragment_workout, container, false);
+
+
     }
 
 }
