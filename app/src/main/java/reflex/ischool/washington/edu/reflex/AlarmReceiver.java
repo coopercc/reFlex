329cc5d0e122571e374bd0f0a7fea83bf976d951
Sarah Loftis <sarahrloftis@gmail.com>
2017-03-04 23:43:28 -0800

    schedule workout date/time

--- app/src/main/java/reflex/ischool/washington/edu/reflex/AlarmReceiver.java
+++ app/src/main/java/reflex/ischool/washington/edu/reflex/AlarmReceiver.java
@@ -4,21 +4,11 @@ package reflex.ischool.washington.edu.reflex;
  * Created by Sarah on 3/1/2017.
  */
 
-    import android.content.BroadcastReceiver;
-    import android.content.Context;
-    import android.content.Intent;
-    import android.app.PendingIntent;
-    import android.util.Log;
-    import android.widget.Toast;
-    import android.net.Uri;
-    import java.util.ArrayList;
-    import android.telephony.SmsManager;
-    import android.app.Notification;
-    import android.app.NotificationManager;
-    import android.content.BroadcastReceiver;
-    import android.content.Context;
-    import android.content.Intent;
-
+import android.content.BroadcastReceiver;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.app.Notification;
+        import android.app.NotificationManager;
 
 public class AlarmReceiver extends BroadcastReceiver {
     public static String NOTIFICATION_ID = "notification-id";
--- app/src/main/java/reflex/ischool/washington/edu/reflex/HomeActivity.java
+++ app/src/main/java/reflex/ischool/washington/edu/reflex/HomeActivity.java
@@ -4,16 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.os.SystemClock;
-import java.util.ArrayList;
-import android.telephony.SmsManager;
-
 
 public class HomeActivity extends Activity {
 
