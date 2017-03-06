8112d51c0627171e5aee6cb2c5b63c8c37b1f432
Cooper Cain <coopercc@uw.edu>
2017-03-06 12:28:50 -0800

    Added rest timer to workout screen

--- ReFlex/app/src/main/java/reflex/ischool/washington/edu/reflex/WorkoutFragment.java
+++ ReFlex/app/src/main/java/reflex/ischool/washington/edu/reflex/WorkoutFragment.java
@@ -3,12 +3,15 @@ package reflex.ischool.washington.edu.reflex;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.CountDownTimer;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,6 +28,10 @@ public class WorkoutFragment extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private List<Exercise> exerciseList;
+    private CountDownTimer aCounter;
+    private TextView timeLeft;
+    private boolean isPaused;
+    private long secondsLeft;
 
     public WorkoutFragment() {
         // Required empty public constructor
@@ -42,6 +49,7 @@ public class WorkoutFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         exerciseList = new ArrayList<Exercise>();
+        secondsLeft = 90;
 
 
         for (int i = 0; i < 3; i++) {
@@ -65,6 +73,46 @@ public class WorkoutFragment extends Fragment {
         adapter = new workoutAdapter(exerciseList, this.getActivity());
         recyclerView.setAdapter(adapter);
 
+        timeLeft = (TextView) rootView.findViewById(R.id.timeLeft);
+
+        final Button start = (Button) rootView.findViewById(R.id.start);
+        aCounter = resumeCountDown(secondsLeft * 1000);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (start.getText().toString().equals("Start")) {
+                    //set to stop, start countdown
+
+                    start.setText("Stop");
+                    if (isPaused) {
+                        aCounter = resumeCountDown(secondsLeft * 1000);
+                    }
+                    aCounter.start();
+                    isPaused = false;
+                } else {
+                    start.setText("Start");
+                    isPaused = true;
+                    aCounter.cancel();
+                    //set to start, end countdown
+                }
+            }
+        });
+
+        final Button reset = (Button) rootView.findViewById(R.id.reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aCounter.cancel();
+                secondsLeft = 90;
+                timeLeft.setText("Rest Seconds Left: 90");
+                aCounter = resumeCountDown(secondsLeft * 1000);
+                if (start.getText().toString().equals("Stop")) {
+                    start.setText("Start");
+                }
+                isPaused = false;
+            }
+        });
+
 
 
         return rootView;
@@ -72,4 +120,25 @@ public class WorkoutFragment extends Fragment {
 
     }
 
+    private CountDownTimer resumeCountDown(long secondsLeft) {
+        return new CountDownTimer(secondsLeft, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                setSecondsLeft(millisUntilFinished / 1000);
+                timeLeft.setText("Rest Seconds Left: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                timeLeft.setText("Rest Time Over!");
+            }
+
+        };
+    }
+
+    private void setSecondsLeft(long secondsLeft) {
+        this.secondsLeft = secondsLeft;
+    }
+
 }
+
+
