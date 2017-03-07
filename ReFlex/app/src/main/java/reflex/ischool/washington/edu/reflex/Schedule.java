053691e14593b08ae77f6183ccbfb641a3b25167
srloftis <sarahrloftis@gmail.com>
2017-03-06 17:42:08 -0800

    Update Schedule.java
    vibrate

--- ReFlex/app/src/main/java/reflex/ischool/washington/edu/reflex/Schedule.java
+++ ReFlex/app/src/main/java/reflex/ischool/washington/edu/reflex/Schedule.java
@@ -110,7 +110,7 @@ public class Schedule extends Activity {
         builder.setContentTitle("Scheduled Notification");
         builder.setContentText(content);
         builder.setSmallIcon(R.drawable.reflex_icon);
-        builder.setVibrate(new long[] {1000, 1000});
+        builder.setVibrate(new long[] {1000, 1000, 1000, 1000, 1000});
         return builder.build();
     }
 }
